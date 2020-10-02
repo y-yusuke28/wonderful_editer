cf587bc91505d2ad9a5ae665fb45f017515d3c97
山口祐輔 <>
2020-10-03 01:21:58 +0900

    rubocopにて追加修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     name { Faker::Name.name }
     sequence(:email) {|n| "#{n}_#{Faker::Internet.email}" }
-    password { Faker::Internet.password(min_length: 7)}
+    password { Faker::Internet.password(min_length: 7) }
 
     trait :with_article do
       article
